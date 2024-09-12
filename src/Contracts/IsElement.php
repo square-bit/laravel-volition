026e510df842149a5a9249e82393176e51c56d59
Tiago Fernandes <tiagoff@gmail.com>
2024-09-12 11:37:41 +0100

    update dependencies + fix stan

--- src/Contracts/IsElement.php
+++ src/Contracts/IsElement.php
@@ -6,7 +6,13 @@ interface IsElement
 {
     public static function getElementType(): string;
 
+    /**
+     * @param array<string, mixed> $payload
+     */
     public static function fromPayload(array $payload): static;
 
+    /**
+     * @return array<string, mixed>
+     */
     public function toPayload(): array;
 }
